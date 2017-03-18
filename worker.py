6831825251c02f37e78f99e28efb41e7027bce60
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-03-18 19:24:37 +0100

    update worker
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- worker.py
+++ worker.py
@@ -1,14 +1,20 @@
 import os
+import urlparse
+from redis import Redis
+from rq import Queue, Connection
+from rq.worker import HerokuWorker as Worker
 
-import redis
-from rq import Worker, Queue, Connection
+listen = ['high', 'default', 'low']
 
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+if not redis_url:
+    raise RuntimeError('Set up Redis To Go first.')
 
-listen = ['default']
-
-conn = redis.from_url(os.getenv('REDIS_URL', 'redis://localhost:6379'))
+urlparse.uses_netloc.append('redis')
+url = urlparse.urlparse(redis_url)
+conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
 
 if __name__ == '__main__':
     with Connection(conn):
-        worker = Worker(list(map(Queue, listen)))
+        worker = Worker(map(Queue, listen))
         worker.work()
