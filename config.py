b06416c9b19d213f9eab7ce34f0736f6073181ac
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-03-18 19:23:51 +0100

    update config init
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'this-really-needs-to-be-changed'
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
