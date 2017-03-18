9e8f81621d5c0dfedccedca54191e6043c675bf0
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-03-19 00:05:05 +0100

    create results model
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- models.py
+++ models.py
@@ -1,2 +1,24 @@
 from app import db
-from sqlalchemy.dialects.postgresql import JSON
+
+
+class Result(db.Model):
+    __tablename__ = 'results'
+
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String())
+    item = db.Column(db.String())
+    agency = db.Column(db.String())
+    fsg = db.Column(db.String())
+    source = db.Column(db.String())
+    title = db.Column(db.String())
+    keywords = db.Column(db.String())
+    url_2 = db.Column(db.String())
+    naics_code = db.Column(db.String())
+    psc_code = db.Column(db.String())
+    contact_info = db.Column(db.String())
+    place_of_performance = db.Column(db.String())
+    link_of_solicitation = db.Column(db.String())
+    description = db.Column(db.String())
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
