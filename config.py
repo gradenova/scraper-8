9d426e9b8093a3392e95e9b0f1f6a537dd95c437
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-03-19 00:04:43 +0100

    update config
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- config.py
+++ config.py
@@ -10,6 +10,9 @@ class Config(object):
     SECRET_KEY = 'this-really-needs-to-be-changed'
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
+    SCRAPER_BASE_URL = (
+        'http://www.mybidmatch.com/go?'
+        'sub=7F604A76-0EF9-48F1-A83F-ABC17511B6FC')
 
 
 class ProductionConfig(Config):
