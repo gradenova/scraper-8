6b527124b370a11b6a46cd160910006075ee58c2
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-03-19 19:19:32 +0100

    update config
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- config.py
+++ config.py
@@ -8,11 +8,14 @@ class Config(object):
     CSRF_ENABLED = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL',
+                                        'mysql://root:@localhost:3306/gfe')
     REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
-    SCRAPER_BASE_URL = (
-        'http://www.mybidmatch.com/go?'
-        'sub=7F604A76-0EF9-48F1-A83F-ABC17511B6FC')
+    BASE_URL = 'http://www.mybidmatch.com'
+    QUERY_URL = '/go?sub=7F604A76-0EF9-48F1-A83F-ABC17511B6FC'
+    SCRAPER_BASE_URL = BASE_URL + QUERY_URL
+
+    RUN_AT = os.getenv('RUN_AT', '17:00 EST')
 
 
 class ProductionConfig(Config):
