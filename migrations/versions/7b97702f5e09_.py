f5d7cd6a0234796dcba38722d914610d0aecd9e8
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-03-19 01:13:46 +0100

    create migration file
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

---
+++ migrations/versions/7b97702f5e09_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 7b97702f5e09
+Revises: 
+Create Date: 2017-03-19 00:09:24.081505
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7b97702f5e09'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
