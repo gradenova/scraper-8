e31d76bd973ac069927f47cfe3ba9ab35f28b0e8
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-03-20 01:22:51 +0100

    update manage.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- manage.py
+++ manage.py
@@ -1,8 +1,9 @@
 import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask_rq2.script import RQManager
 
-from app import app, db
+from app import app, db, rq
 
 
 app.config.from_object(os.getenv('APP_SETTINGS', 'config.DevelopmentConfig'))
@@ -11,6 +12,7 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+manager.add_command('rq', RQManager(rq))
 
 
 if __name__ == '__main__':
