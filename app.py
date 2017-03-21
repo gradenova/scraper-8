30979a64f433676a9f932d7600dad8464c2bb9e3
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-03-21 10:44:09 +0100

    update app.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- app.py
+++ app.py
@@ -4,11 +4,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 import rq_dashboard
-
-from rq import Queue
-from rq.job import Job
-
-from worker import conn
 from utils import add_basic_auth
 
 app = Flask(__name__)
@@ -17,18 +12,11 @@ db = SQLAlchemy(app)
 
 app.config.from_object(os.getenv('APP_SETTINGS', 'config.DevelopmentConfig'))
 
-q = Queue(connection=conn)
-
 if not app.config.get('TESTING'):
     add_basic_auth(rq_dashboard.blueprint,
                    os.getenv('USER', 'admin'), os.getenv('PASSWORD', '[]'))
 
 app.register_blueprint(rq_dashboard.blueprint, url_prefix='/monitor')
-
-
-def test():
-    print('sknsjkfnknk')
-    return 'Hello world, sample tasks.'
 
 
 @app.route('/')
@@ -37,7 +25,4 @@ def hello():
 
 
 if __name__ == '__main__':
-    from scraper import scraper
-    job = q.enqueue_call(func=test, result_ttl=5000)
-    print(job.id)
     app.run()
